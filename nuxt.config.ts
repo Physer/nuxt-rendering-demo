8dfd10bbccd99e5847fe1a59126659c558c5cf97
Alex Schouls <alex.schouls@valtech.com>
2023-04-21 14:54:58 +0200

    updated configuration settings to use it in the browser

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    public: {
+      contentstackApikey: "",
+      contentstackDeliverytoken: "",
+      contentstackEnvironment: "dev",
+    },
+  },
+});
--- utils/config.ts
+++
@@ -1,5 +0,0 @@
-export const contentstackConfiguration = {
-  apiKey: process.env.CONTENTSTACK_APIKEY || "",
-  deliveryToken: process.env.CONTENTSTACK_DELIVERYTOKEN || "",
-  environment: process.env.CONTENTSTACK_ENVIRONMENT || "dev",
-};
