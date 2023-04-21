8ab1e2ec86201f594b5c6b1ae72801ac9d5d4f06
Alex Schouls <alex.schouls@valtech.com>
2023-04-21 14:55:05 +0200

    added data fetcher

--- app.vue
+++ app.vue
@@ -3,3 +3,7 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup="ts">
+const contentPage = await fetchContentPages();
+</script>
---
+++ utils/fetcher.ts
@@ -0,0 +1,14 @@
+import { Stack } from "contentstack";
+
+export async function fetchContentPages() {
+  const config = useRuntimeConfig();
+  const stack = Stack({
+    api_key: config?.public?.contentstackApikey,
+    delivery_token: config?.public?.contentstackDeliverytoken,
+    environment: config?.public?.contentstackEnvironment,
+  });
+  const contentType = "schouls_content_page";
+  const query = stack.ContentType(contentType).Query();
+  const result = await query.toJSON().find();
+  return "";
+}
