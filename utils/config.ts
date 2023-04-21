b1141830e068ddb4ff52fdea98b6c926a4b7aa81
Alex Schouls <alex.schouls@valtech.com>
2023-04-21 09:11:29 +0200

    added configuration helper

---
+++ utils/config.ts
@@ -0,0 +1,5 @@
+export const contentstackConfiguration = {
+  apiKey: process.env.CONTENTSTACK_APIKEY || "",
+  deliveryToken: process.env.CONTENTSTACK_DELIVERYTOKEN || "",
+  environment: process.env.CONTENTSTACK_ENVIRONMENT || "dev",
+};
