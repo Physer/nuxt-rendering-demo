c164168e6916de40804b3675a06b82bd3d62b39b
Alex Schouls <alex.schouls@valtech.com>
2023-05-02 09:45:10 +0200

    added index with route overview

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Select a page:</p>
+    <li v-for="route in routes">
+      <a :href="route">{{ route }}</a>
+    </li>
+  </div>
+</template>
+
+<script setup lang="ts">
+const routes = await buildRoutes([contentTypes.contentPage]);
+</script>
