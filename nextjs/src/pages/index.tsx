a4da46d50b584020b1def062869412c0f8684b77
Alex Schouls <alex.schouls@valtech.com>
2023-05-04 17:03:25 +0200

    created homepage with all links

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import Link from 'next/link';
+import { ContentPage, getContentPages } from '../../utils/pages';
+
+export default function Home({ pages }: any) {
     return (
         <>
-            <div>Hello SSG!</div>
+            <div>
+                <p>Select a page:</p>
+                <ul>
+                    {pages.map((page: ContentPage) => (
+                        <li key={page.url}>
+                            <Link key={page.url} href={page.url}>
+                                {page.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
+}
+
+export async function getStaticProps() {
+    const pages = await getContentPages();
+    return { props: { pages } };
 }
